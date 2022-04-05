cbeb9898237ae5dcf7acae883b3a52c34ac49487
SanyFaysal <tfoysalahmed@gmail.com>
2022-04-05 11:41:55 +0600

    custom link added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,7 +3,7 @@ import ChartInLine from '../Chart/ChartInLine';
 import ChartInBar from '../Chart/ChartInBar';
 const Dashboard = () => {
     return (
-        <div className='lg:container md:container  mx-auto flex lg:flex-row lg:justify-between md:flex-col md:justify-center sm:flex-col sm:justify-center flex-col justify-center'>
+        <div className='lg:container md:container mx-auto flex lg:flex-row lg:justify-between md:flex-col md:justify-center sm:flex-col sm:justify-center flex-col justify-center'>
             <ChartInLine></ChartInLine>
             <ChartInBar></ChartInBar>
         </div>
